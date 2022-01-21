7f49ee727bcbd18219e5fec61c44fff0551f12f1
Bartek Dobrowolski-Nowakowski <bartek@bnowakowski.pl>
2022-01-21 11:49:33 +0100

    add rsync with and without download directory

--- rsync-back-with-change-of-permission.sh
+++ rsync-back-with-change-of-permission.sh
@@ -2,5 +2,7 @@
 
 #vagrant plugin install vagrant-rsync-back
 vagrant ssh -c 'sudo chmod -R 777 /mnt/ubu-storage/Plex/'
-vagrant rsync-back
+
+VAGRANT_VAGRANTFILE=Vagrantfile vagrant rsync-back
+#VAGRANT_VAGRANTFILE=Vagrantfile.full-rsync vagrant rsync-back
 
